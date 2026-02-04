01ae223412c1cc22c4b19fcdf83a56b370c28a35
Msadauki <muhdsadauki2000@gmail.com>
2026-02-04 14:35:49 +0100

    Update examModel.js

--- src/models/examModel.js
+++ src/models/examModel.js
@@ -1,4 +1,4 @@
-const pool = require('../config/dbConfig');
+const pool = require('../config/db');
 
 async function createExam({ title, sessionId, duration, allowCalculator = false }) {
   const query = `
