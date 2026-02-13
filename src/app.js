064f43019251fb6a80f078d38eb918d9e299009a
Msadauki <muhdsadauki2000@gmail.com>
2026-02-13 09:35:41 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-
 const app = express();
 
+const authRoutes = require('./routes/auth');
+
 app.use(express.json());
+
+// Mount auth routes
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).send('ILMIO backend live');
